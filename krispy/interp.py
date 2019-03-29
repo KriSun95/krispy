7b3bb13764094007dfb3d6f353c53562b9595a92
Kristopher Cooper <k.cooper.2@research.gla.ac.uk>
2019-03-29 11:56:45 +0000

    change how the interpolation function works.

--- krispy/interp.py
+++ krispy/interp.py
@@ -4,22 +4,23 @@ Functions to go in here (I think!?):
     ~
 
     KC: 28/03/2019, added-
-    ~find_my_x()
+    ~find_my_y()
 '''
 #no dependencies
 
 '''
 Alterations:
-    KC: **/**/**** - .
+    KC: 29/03/2019 - changed find_my_x() to find_my_y().
 '''
 
-def find_my_x(your_y, data_x, data_y):
-    """Takes an input y, linear interpolates the data and produces a corresponding x.
+#interpolation function
+def find_my_y(your_x, data_x, data_y):
+    """Takes an input x, linear interpolates the data and produces a corresponding y(s).
     
     Parameters
     ----------
-    your_y : float
-            A single number, of which you want the corresponding x value(s) of through linear interpolation of the data 
+    your_x : float
+            A single number, of which you want the corresponding y value(s) of through linear interpolation of the data 
             given (data_x, data_y).
     
     data_x : 1-d list/array
@@ -30,33 +31,33 @@ def find_my_x(your_y, data_x, data_y):
             
     Returns
     -------
-    A list of corresponding x(s) to the input your_y.
+    A list of corresponding y(s) to the input your_x.
     """
     
-    your_y_between = []
+    your_x_between = []
     #search for y range which has your point in it
-    for dy in range(len(data_y)-1):
-        if dy == 0: #so the first one isnt completely discounted
-            if data_y[dy] <= your_y <= data_y[dy+1]:
+    for dx in range(len(data_x)-1):
+        if dx == 0: #so the first one isnt completely discounted
+            if data_x[dx] <= your_x <= data_x[dx+1]:
                 #append the coordinates of the range
-                your_y_between.append([[data_x[dy], data_y[dy]], [data_x[dy+1], data_y[dy+1]]])
+                your_x_between.append([[data_x[dx], data_y[dx]], [data_x[dx+1], data_y[dx+1]]])
         else:
-            if (data_y[dy] < your_y <= data_y[dy+1]) or (data_y[dy] > your_y >= data_y[dy+1]):
-                your_y_between.append([[data_x[dy], data_y[dy]], [data_x[dy+1], data_y[dy+1]]])
+            if (data_x[dx] < your_x <= data_x[dx+1]) or (data_x[dx] > your_x >= data_x[dx+1]):
+                your_x_between.append([[data_x[dx], data_y[dx]], [data_x[dx+1], data_y[dx+1]]])
             
-    #no extrapolation, if your_y is not within the set of y's given (data_y) then this won't work    
-    if your_y_between != []: 
-        print('Your y is out of range of this data_y.')
+    #no extrapolation, if your_x is not within the set of x's given (data_x) then this won't work    
+    if your_x_between == []: 
+        print('Your x is out of range of this data_x.')
         return
     
-    #make a straight line betwen the points and plug your y value in
-    found_x = []
-    for coords in your_y_between:
+    #make a straight line betwen the points and plug your x value in
+    found_y = []
+    for coords in your_x_between:
         coord1 = coords[0]
         coord2 = coords[1]
         grad = (coord1[1] - coord2[1]) / (coord1[0] - coord2[0])
-        _found_x = ( (your_y - coord1[1]) / grad) + coord1[0]
-        found_x.append(_found_x)
+        _found_y = grad * (your_x - coord1[0]) + coord1[1]
+        found_y.append(_found_y)
     
-    #return all the x's found, no guarentee the there is a one-to-one mapping
-    return found_x
+    #return all the y's found, no guarentee the there is a one-to-one mapping
+    return found_y
