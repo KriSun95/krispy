94db9bf71202fb21030434c603547f1f8cf60d6b
Kristopher Cooper <k.cooper.2@research.gla.ac.uk>
2019-10-08 16:19:29 +0100

    interaction plot code looks a little more sane now.

--- krispy/interaction_plot.py
+++ krispy/interaction_plot.py
@@ -1,33 +1,33 @@
 from matplotlib import pyplot as plt
 
+class LineBuilder:
+    def __init__(self, axis=None):
+        line, = axis.plot([], [], marker='x', linestyle='-', color='red') 
+        self.line = line
+        self.xs = list(line.get_xdata())
+        self.ys = list(line.get_ydata())
+        self.cid = line.figure.canvas.mpl_connect('button_press_event', self)
+
+    def __call__(self, event):
+        if event.inaxes!=self.line.axes: return
+        self.xs.append(event.xdata)
+        self.ys.append(event.ydata)
+        self.line.set_data(self.xs, self.ys)
+        self.line.figure.canvas.draw() #_idle
+
+    def get_xy(self):
+        return self.xs, self.ys
+
 def draw_lines(data):
-
-    class LineBuilder:
-        def __init__(self):
-            line, = ax.plot([], [], marker='x', linestyle='-', color='red') 
-            self.line = line
-            self.xs = list(line.get_xdata())
-            self.ys = list(line.get_ydata())
-            self.cid = line.figure.canvas.mpl_connect('button_press_event', self)
-
-        def __call__(self, event):
-            if event.inaxes!=self.line.axes: return
-            self.xs.append(event.xdata)
-            self.ys.append(event.ydata)
-            self.line.set_data(self.xs, self.ys)
-            self.line.figure.canvas.draw() #_idle
-
-        def get_xy(self):
-            return self.xs, self.ys
 
     if len(data) == 0:
         data = [[1,2,3], [4,5,6], [7,8,9]]
 
     fig = plt.figure(1)
     ax = fig.add_subplot(111)
-    ax.set_title('click to build line segments')
+    ax.set_title('Click to build line segments')
     plt.imshow(data, origin='lower')
-    linebuilder = LineBuilder()
+    linebuilder = LineBuilder(axis=ax)
     plt.show()
 
     while plt.fignum_exists(1): #while plot is being drawn on don't continue
